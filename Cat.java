3425cb28904290991094eb45fb9ac18dc0331c19
Mohammad Jihad Uddin <mjihaduddin00@outlook.com>
2021-03-04 15:20:46 -0600

    Main File
    I am committing the main file that the application is comprised of (Java File).

---
+++ Cat.java
@@ -0,0 +1,18 @@
+/* 
+What's the cat's name?
+
+*/
+
+public class Cat {
+    private String name = "nameless cat";
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.setName("Simba");
+        System.out.println(cat.name);
+    }
+}
